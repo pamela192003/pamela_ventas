caebec8b1cb620d0fa7e85e27b477630003eea00
wilmerMendoza <rojasataupillcopamels@gmail.com>
2025-07-17 17:42:59 -0500

    Update header.php

--- views/include/header.php
+++ views/include/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>STREETWEAR X | Moda Urbana Joven</title>
+    <title>ZAPATERIA MENDOZA | Moda Urbana Joven</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
@@ -295,11 +295,12 @@
                                 <i class="fas fa-person nav-icon"></i>Urban Men
                             </a>
                         </li>
+                        <!-- 
                         <li class="nav-item">
                             <a class="nav-link" href="<?php echo BASE_URL ?>mujeres">
                                 <i class="fas fa-person-dress nav-icon"></i>Hype Women
                             </a>
-                        </li>
+                        </li>-->
                     </ul>
 
                     <div class="d-flex align-items-center">
